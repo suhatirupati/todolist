9db57b16b1fe26c554dbaeb2e5ba943d466094d8
suhatirupati <126375420+suhatirupati@users.noreply.github.com>
2023-04-28 19:51:31 +0530

    editing the list

--- src/ListItems.js
+++ src/ListItems.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import './ListItems.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import FlipMove from 'react-flip-move';
 
 function ListItems(props) {
     const items = props.items;
     const listItems = items.map(item => {
-        return <div className='list' key={item.key}>
+        return (
+        <FlipMove>
+        <div className='list' key={item.key}>
+             
         <p>
             <input type='text' id={item.text}
-            value={item.text} 
+            value={item.text}
+            onChange={e => props.setUpdate(e.target.value, item.key)} 
             />
         <span>
             <FontAwesomeIcon className='faicons' icon='trash' 
@@ -17,10 +22,14 @@ function ListItems(props) {
         </span>
         </p>    
     </div>
-    })
+    </FlipMove>
+    );
+    });
   return (
     <div>{listItems}</div>
   )
-}
+} 
+
+
 
 export default ListItems
